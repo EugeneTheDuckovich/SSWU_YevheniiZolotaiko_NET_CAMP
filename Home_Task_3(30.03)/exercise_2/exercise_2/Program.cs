2d99ceaca34b599ad58cd5aee297c626a39b7977
Eugene <zhenyagold1@gmail.com>
2023-04-03 14:30:08 +0300

    Hometask 3

---
+++ Home_Task_3(30.03)/exercise_2/exercise_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using exercise_2;
+
+while (true)
+{
+    Console.WriteLine("Enter string to work with or input nothing to finish the program");
+    var workString = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(workString)) break;
+
+    Console.WriteLine($"number of words that start with capital letter: {workString!.CapitalWordsAmount()}");
+
+    Console.WriteLine();
+    Console.WriteLine("input string, which second occurance index you want to find:");
+    var secondOccuranceString = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(secondOccuranceString)) continue;
+    var secondIndex = workString.SecondIndexOf(secondOccuranceString);
+
+    if (secondIndex is null) Console.WriteLine("there is no second occurance of this substring");
+    else Console.WriteLine($"index of second occurance: {secondIndex}");
+
+    Console.WriteLine();
+    Console.WriteLine("input replacer for strings with doubled letters:");
+    var replacer = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(replacer)) continue;
+
+    Console.WriteLine($"string with replaced words: {workString.ReplaceDoubleLetters(replacer)}");
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine();
+}
---
+++ Home_Task_3(30.03)/exercise_2/exercise_2/StringsHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_2;
+
+public static class StringsHelper
+{
+    public static int? SecondIndexOf(this string str, string subString)
+    {
+        var secondIndex = str.IndexOf(subString,str.IndexOf(subString) + subString.Length);
+
+        return secondIndex >= 0 ? secondIndex : null;
+    }
+
+    public static int CapitalWordsAmount(this string str)
+    {
+        var splittedString = str.Split(' ');
+        return splittedString.Where(s => char.IsUpper(s[0])).Count();
+    }
+
+    public static string ReplaceDoubleLetters(this string str, string replacer)
+    {
+        var splittedString = str.Split(' ');
+
+        for (int i = 0; i < splittedString.Length; i++)
+        {
+            for (int j = 0; j < splittedString[i].Length - 1; j++)
+            {
+                if (!char.IsLetter(splittedString[i][j])) continue;
+
+                if (splittedString[i][j] == splittedString[i][j+1])
+                {
+                    splittedString[i] = replacer;
+                }
+            }
+        }
+
+        return string.Join(" ", splittedString);
+    }
+}
